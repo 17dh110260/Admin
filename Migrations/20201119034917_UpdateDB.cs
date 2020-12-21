58186fe3f09c7509e8f476def572df1add11a94d
17dh110260 <64671767+17dh110260@users.noreply.github.com>
2020-12-21 09:40:21 +0700

    TMDT
    TMDT

---
+++ Migrations/20201119034917_UpdateDB.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeShopping.Migrations
+{
+    public partial class UpdateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1e4b0354-69eb-4b9f-955d-8ead3b911aec");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "68f56f0c-1cd2-498c-aa95-73e9c97c2d57");
+
+            migrationBuilder.DropColumn(
+                name: "ProductImage",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "OrderDetails");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "01ca9ae9-4346-407d-aec5-57b0484825e9", "3aa7c57e-6023-4837-b574-c0fb569de609", "Visitor", "VISITOR" },
+                    { "c994d7c7-956f-44ea-a242-046e4bef0697", "9172f92d-3322-4868-a0bc-156be6a2ac5b", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 958, DateTimeKind.Local).AddTicks(4065));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 959, DateTimeKind.Local).AddTicks(4938));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 959, DateTimeKind.Local).AddTicks(5012));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 959, DateTimeKind.Local).AddTicks(5015));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "01ca9ae9-4346-407d-aec5-57b0484825e9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c994d7c7-956f-44ea-a242-046e4bef0697");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductImage",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "68f56f0c-1cd2-498c-aa95-73e9c97c2d57", "c3a52a38-2a38-439e-b097-a06bbd93844b", "Visitor", "VISITOR" },
+                    { "1e4b0354-69eb-4b9f-955d-8ead3b911aec", "af33f1bc-32cd-43ab-9e02-4bfc2aa5fbdf", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 40, 3, 430, DateTimeKind.Local).AddTicks(4644));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 40, 3, 432, DateTimeKind.Local).AddTicks(665));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 40, 3, 432, DateTimeKind.Local).AddTicks(784));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 40, 3, 432, DateTimeKind.Local).AddTicks(788));
+        }
+    }
+}
---
+++ Migrations/20201119035101_addStatus.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeShopping.Migrations
+{
+    public partial class addStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "01ca9ae9-4346-407d-aec5-57b0484825e9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c994d7c7-956f-44ea-a242-046e4bef0697");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "OrderDetails",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "39b99083-f75d-4ce1-948f-26639779c22c", "88218560-5fbb-4938-b4b7-caa30b950d48", "Visitor", "VISITOR" },
+                    { "3dc7af9b-0c36-4829-a8a6-8736fffd369f", "f6477788-79f7-4a7a-ae04-73d82a2956a7", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 51, 1, 139, DateTimeKind.Local).AddTicks(306));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 51, 1, 140, DateTimeKind.Local).AddTicks(1443));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 51, 1, 140, DateTimeKind.Local).AddTicks(1507));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 51, 1, 140, DateTimeKind.Local).AddTicks(1510));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "39b99083-f75d-4ce1-948f-26639779c22c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3dc7af9b-0c36-4829-a8a6-8736fffd369f");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "OrderDetails");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "01ca9ae9-4346-407d-aec5-57b0484825e9", "3aa7c57e-6023-4837-b574-c0fb569de609", "Visitor", "VISITOR" },
+                    { "c994d7c7-956f-44ea-a242-046e4bef0697", "9172f92d-3322-4868-a0bc-156be6a2ac5b", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 958, DateTimeKind.Local).AddTicks(4065));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 959, DateTimeKind.Local).AddTicks(4938));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 959, DateTimeKind.Local).AddTicks(5012));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2020, 11, 19, 10, 49, 16, 959, DateTimeKind.Local).AddTicks(5015));
+        }
+    }
+}
